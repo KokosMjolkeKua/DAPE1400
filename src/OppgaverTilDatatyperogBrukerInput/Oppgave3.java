369dbe097197d525bd1e26a44960c4f42a9241f0
KokosMjolkeKua <insmi6764@oslomet.no>
2025-09-03 14:00:09 +0200

    extra modules from school

---
+++ src/OppgaverTilDatatyperogBrukerInput/Oppgave3.java
@@ -0,0 +1,12 @@
+package OppgaverTilDatatyperogBrukerInput;
+import static javax.swing.JOptionPane.*;
+public class Oppgave3 {
+    public static void main(String[] args){
+        showMessageDialog(null, "Halla, hvor mange dager har du i live tro?");
+        double alder;
+        alder = Double.parseDouble(showInputDialog("Hva er din alder?"));
+        double dager = alder * 365;
+        showMessageDialog(null, "Du har vært i live i " + dager + " dager.");
+    }
+
+}
---
+++ src/OppgaverTilDatatyperogBrukerInput/Oppgave4.java
@@ -0,0 +1,4 @@
+package OppgaverTilDatatyperogBrukerInput;
+
+public class Oppgave4 {
+}
