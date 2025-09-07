30555d814671a39ebd6144a4937b54023d511fba
KokosMjolkeKua <insmi6764@oslomet.no>
2025-09-07 13:44:13 +0200

    extra modules from school

--- src/Aritmetikk/AritmetikklogiskeUtrykk.java
+++ src/Aritmetikk/AritmetikklogiskeUtrykk.java
@@ -2,6 +2,18 @@ package Aritmetikk;
 import static javax.swing.JOptionPane.*;
 public class AritmetikklogiskeUtrykk {
     public static void main(String[] args) {
+        String text = "blah";
+        int tall = 42;
+        double decimaltall = 4.2;
+        boolean sannhetsverdi = true;
+
+        int number1 = 5;
+        int number2 = 2;
+        int integerQuotient = 5/2;
+        int integerModulus = 5%2;
+        System.out.println(number1 + number2 + " " + integerQuotient + " " + integerModulus);
+
+
 
     }
 }
