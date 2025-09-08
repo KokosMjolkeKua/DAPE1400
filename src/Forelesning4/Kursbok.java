a3bb287ff2f538c9f90981a22ff23a5668534987
KokosMjolkeKua <insmi6764@oslomet.no>
2025-09-08 13:25:19 +0200

    extra modules from school

---
+++ src/Forelesning4/Kursbok.java
@@ -0,0 +1,7 @@
+package Forelesning4;
+import static javax.swing.JOptionPane.*;
+public class Kursbok {
+    public static void main(String[]args){
+        showMessageDialog(null, "Kursbok");
+    }
+}
