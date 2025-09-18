9fb3047d4425393b3aa8223af18388d3f6d89c61
KokosMjolkeKua <insmi6764@oslomet.no>
2025-09-18 12:03:47 +0200

    extra modules from school

--- src/OppgaverKlasserOgObjekter/Oppgave5.java
+++ src/OppgaverKlasserOgObjekter/Oppgave5.java
@@ -8,8 +8,29 @@ class Kalkulator{
         this.tall1 = tall1;
         this.tall2 = tall2;
 
-        tall1 = double.parsedouble(showInputDialog("   "));
-        tall2 = double.parsedouble(showInputDialog(""));
+        //tall1 = double.parsedouble(showInputDialog(null,"Skriv inn ett tall:"));
+        //tall2 = double.parsedouble(showInputDialog(null,"Skriv inn ett tall til:"));
+    }
+    public void subtrahere(){
+        this.tall1 = tall1;
+        this.tall2 = tall2;
+
+        //tall1 = double.parsedouble(showInputDialog(null,"Skriv inn ett tall:"));
+        //tall2 = double.parsedouble(showInputDialog(null,"Skriv inn ett tall til:"));
+    }
+    public void multiplisere(){
+        this.tall1 = tall1;
+        this.tall2 = tall2;
+
+        //tall1 = double.parsedouble(showInputDialog(null,"Skriv inn ett tall:"));
+        //tall2 = double.parsedouble(showInputDialog(null,"Skriv inn ett tall til:"));
+    }
+    public void dividere(){
+        this.tall1 = tall1;
+        this.tall2 = tall2;
+
+        //tall1 = double.parsedouble(showInputDialog(null,"Skriv inn ett tall:"));
+        //tall2 = double.parsedouble(showInputDialog(null,"Skriv inn ett tall til:"));
     }
 }
 
